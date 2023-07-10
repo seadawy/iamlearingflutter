106ceefd815993e5c19c2362b09b58b3bed4f702
seadawy <k9335551@gmail.com>
2023-07-10 11:35:27 +0300

    new

--- BMI.dart
+++ BMI.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class BMI extends StatelessWidget {
+  const BMI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("BMI Calculator"),
+        ),
+      ),
+      body: Container(
+        color: Colors.deepPurple[800],
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    color: Colors.deepPurple[500],
+                    child: Column(
+                      children: [Icon(Icons.male), Text("MALE")],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [Icon(Icons.female), Text("FEMALE")],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
