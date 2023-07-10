27f2ac3aef27446c452721628e067f22aa0009b4
seadawy <k9335551@gmail.com>
2023-07-10 03:47:54 +0300

    counter

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
-import 'package:app1/userscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:app1/counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: User(),
+      home: Counter(),
     );
   }
 }
